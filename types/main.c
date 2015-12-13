459b93def47206303cb81cbd0cb95c3be15a4731
Gaël Foppolo <me@gaelfoppolo.com>
2015-12-14 00:35:21 +0100

    shape translation

--- types/main.c
+++ types/main.c
@@ -3,19 +3,19 @@
 
 int main(int argc, char const *argv[])
 {
-	Tree* a = createLeaf("Triangle rectangle", 7);
-	Tree* b = createLeaf("Triangle équilatéral", 11);
-	Tree* c = createLeaf("Trapèze", 9);
-	Tree* d = createLeaf("Carré", 12);
-	Tree* e = createLeaf("Cercle", 5);
+	Tree* a = createLeaf("Rectangle triangle", 7);
+	Tree* b = createLeaf("Equilateral triangle", 11);
+	Tree* c = createLeaf("Trapeze", 9);
+	Tree* d = createLeaf("Square", 12);
+	Tree* e = createLeaf("Circle", 5);
 	Tree* f = createLeaf("Ellipse", 6);
-	Tree* g = createNode("Triangle isocèle", 8, b, NULL);
+	Tree* g = createNode("Isosceles triangle", 8, b, NULL);
 	Tree* h = createNode("Rectangle", 10, d, NULL);
-	Tree* i = createNode("Ellipsoïde", 2, e, f);
+	Tree* i = createNode("Ellipsoid", 2, e, f);
 	Tree* j = createNode("Triangle", 3, a, g);
-	Tree* k = createNode("Quadrilatère", 4, c, h);
+	Tree* k = createNode("Quadrilateral", 4, c, h);
 	Tree* l = createNode("Polygone", 1, j, k);
-	Tree* m = createNode("Quelconque", 0, l, i);
+	Tree* m = createNode("Whatever", 0, l, i);
 
 	Tree* n = LCA(m, b, h);
 	printf("%s\n", n->name);
