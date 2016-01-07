9dbfd6ef4833767674f4ebb36e94b8c255e4b3dc
Ebatsin <bast.esca@gmail.com>
2016-01-07 17:00:00 +0100

    attribute types definition

---
+++ types/attributes-types.h
@@ -0,0 +1,8 @@
+#ifndef _ATTRIBUTE_TYPES_H_
+#define _ATTRIBUTE_TYPES_H_
+
+#define TYPE_INT 1 // attribute that represents an integer
+#define TYPE_ENUM 2 // attribute that represents an enumeration
+#define TYPE_TREE 3 // attribute that represents a tree
+
+#endif // _ATTRIBUTE_TYPES_H_
