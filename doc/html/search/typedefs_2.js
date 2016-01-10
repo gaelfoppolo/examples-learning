535500f4d5caa76920b19a460070af6e1b703619
Gaël Foppolo <me@gaelfoppolo.com>
2016-01-10 22:30:26 +0100

    add .gitignore

---
+++ doc/html/search/typedefs_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['interval',['Interval',['../interval_8h.html#a506b375dfe936bbe70e79b44be526738',1,'interval.h']]]
+];
---
+++ doc/html/search/variables_7.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['second',['second',['../struct____basic__string__pair.html#afda7669ed771f9f76cd52c19d299c1ad',1,'__basic_string_pair']]],
+  ['str',['str',['../struct____basic__enum.html#ad580200e599931c086006513b3a5a927',1,'__basic_enum::str()'],['../struct____tree.html#a356fa3e93ab5e39c4b58b444d81cec9d',1,'__tree::str()']]]
+];
