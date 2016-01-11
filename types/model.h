b78dfe4130d994138cf16a353dc329579f945587
Ebatsin <bast.esca@gmail.com>
2016-01-11 15:31:06 +0100

    fix Model fields

--- types/model.h
+++ types/model.h
@@ -13,13 +13,21 @@
 #include "vector.h"
 
 /**
+*	@brief The attribute possible value and its name
+*	@field mt The possible value of the attribute
+*	@field name The name of the attribute
+*/
+typedef struct __basic_model_attribute {
+	ModelType mt;
+	char* name;
+} ModelAttribute;
+
+/**
 *   @brief Structure that contains the model
-*   @field mt The type of the propertie's model
-*   @field name The name of the propertie's model
+*   @field ma The vector of attributes possible values and their name
 */
 typedef struct __basic_model {
-    Vector(ModelType) mt;
-    char* name;
+    Vector(ModelAttribute) ma;
 } Model;
 
 #endif // _MODEL_H_
