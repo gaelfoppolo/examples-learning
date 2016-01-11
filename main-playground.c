56a122ab25fca0d8363a2a30cb1787356b6cc037
Gaël Foppolo <me@gaelfoppolo.com>
2016-01-11 15:35:02 +0100

    update main-playground

--- main-playground.c
+++ main-playground.c
@@ -2,25 +2,118 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "types/out-enum.c"
+
+/**
+*	@brief define a dynamic array of type t
+*/
+#define Vector(t) struct {int size, capacity; t *data; }
+
+/**
+*	@brief Init the vector
+*	@param vect The vector to be initialized
+*/
+#define vectInit(vect) ((vect).size = (vect).capacity = 0, (vect).data = 0)
+
+/**
+*	@brief Free the vector
+*	@param vect The vector to be initialized
+*/
+#define vectFree(vect) free((vect).data)
+
+/**
+*	@brief Returns the element at a certain index of the array
+*	@param vect The vector of which to access the element
+*	@param index The index of the element (between 0 and vector size - 1)
+*	@return The element (directly. You can use this as a left value)
+*/
+#define vectAt(vect, index) ((vect).data[(index)])
+
+/**
+*	@brief Return the size of the array
+*	@param vect The array of which to get the size
+*	@return The size of the array
+*/
+#define vectSize(vect) ((vect).size)
+
+/**
+*	@brief Append an element at the end of the vector
+*	@param type The type of the element (must be the same type as the other elements of the array)
+*	@param vect The vector at the end of which to add the element
+*	@param value The element to be appened
+*/
+#define vectPush(type, vect, value) do {                                                                                \
+                                        if((vect).size == (vect).capacity) {                                            \
+                                            (vect).capacity = ((vect).capacity ? (vect).capacity * 2 : 10);             \
+                                            (vect).data = (type*)realloc((vect).data, sizeof(type) * (vect).capacity);  \
+                                        }                                                                               \
+                                        (vect).data[(vect).size++] = value;                                             \
+                                    } while(0)
+
+/**
+*	@brief Search for an element in the vector. Returns its index if found
+*	@param vect The vector to search in
+*	@param value The value to search for
+*	@param out An integer that will hold the return value (either the index if found ou -1 if the element is not int the vector)
+*/
+#define vectIndexOf(vect, value, out) do {																					\
+										out = -1;																		\
+										for(int i = 0; i < vectSize(vect); ++i) {										\
+											if(vectAt(vect, i) == value) {												\
+												out = i;																\
+												break;																	\
+											}																			\
+										}																				\
+									} while(0)
+
+/**
+*   @brief All the integer composing the enum
+*   @field oenu The list of the solution's integers
+*/
+typedef struct __basic_outenum {
+    Vector(int) oenu;
+} OutEnum;
+
+/**
+*	@brief Init the output enum
+*	@param enu A pointer to the enum to init
+*/
+void initOutEnum(OutEnum* oenu);
+
+/**
+*	@brief Free the ouput enum previously initialized by initEnum
+*	@param oenu A pointer to the enum to free
+*/
+void freeOutEnum(OutEnum* oenu);
+
+void initOutEnum(OutEnum* oenu) {
+	vectInit(oenu->oenu);
+}
+
+void freeOutEnum(OutEnum* oenu) {
+	vectFree(oenu->oenu);
+	free(oenu);
+}
 
 int main(int argc, char const *argv[])
 {
 	OutEnum* test = (OutEnum*)malloc(sizeof(OutEnum));
 	initOutEnum(test);
 
-	int* x;
+	int* x = (int *)malloc(sizeof(int));;
+	*x = 10;
 
 	vectPush(int, test->oenu, 1);
 	vectPush(int, test->oenu, 4);
 	vectPush(int, test->oenu, 12);
 	vectPush(int, test->oenu, 5);
 
-	vectIndexOf(test->oenu, 4, *x);
+	vectIndexOf(test->oenu, 12, *x);
+	printf("%d\n", *x);
 
+	vectIndexOf(test->oenu, 234, *x);
 	printf("%d\n", *x);
 
 	freeOutEnum(test);
-	free(test);
+	free(x);
 	return 0;
 }
