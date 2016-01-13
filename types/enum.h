f305affba7499cc26b94f916ccde9f027b92a3ca
Gaël Foppolo <me@gaelfoppolo.com>
2016-01-13 17:51:01 +0100

    add EnumModel structure

--- types/enum.h
+++ types/enum.h
@@ -9,26 +9,20 @@
 #ifndef _ENUM_H_
 #define _ENUM_H_
 
+#include "vector.h"
+
 /**
 *   @brief Structure that contains the enum attributes
 *   @field id The unique identifier of the enum
 *   @field str String matching the unique id
 */
-typedef struct __basic_enum {
+typedef struct __basic_type_enum {
     int id;
     char* str;
+} EnumType;
+
+typedef struct __basic_enum {
+	Vector(EnumType) enu;
 } Enum;
-
-/**
-*	@brief Get the string from the identifier
-*	@param id The identifier
-*/
-char* getString(int id);
-
-/**
-*	@brief Get the identifier from the string
-*	@param str The string
-*/
-int getIdentifier(char* str);
 
 #endif // _ENUM_H_
