667096d01d1f4f03f212484a1473b3491280d72e
Gaël Foppolo <me@gaelfoppolo.com>
2016-01-30 17:36:00 +0100

    add type to OutObject & freeOutObject method

--- types/out-object.h
+++ types/out-object.h
@@ -10,6 +10,7 @@
 #define _OUTOBJECT_H_
 
 #include "interval.h"
+#include "attribute-types.h"
 #include "out-enum.h"
 
 /**
@@ -17,11 +18,14 @@
 *   @field union (inter or oenum or tree) The attribute depending on the type
 */
 typedef struct __basic_outobject {
-    union {
+    attrType type;
+	union {
     	Interval inter;
     	OutEnum oenu;
     	int tree;
     };
 } OutObject;
+
+void freeOutObject(OutObject* oo);
 
 #endif // _OUTOBJECT_H_
