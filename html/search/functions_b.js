9ff52131745f12a975687bf7d12bc308986a99df
Gaël Foppolo <me@gaelfoppolo.com>
2016-02-09 11:32:23 +0100

    fix doxygen config, add latex doc output & update makefile

--- html/search/functions_b.js
+++ html/search/functions_b.js
@@ -5,9 +5,9 @@ var searchData=
   ['parseattrtypeenum',['parseAttrTypeEnum',['../parsers_8h.html#ab456bb242016ec949b25498e0a506fad',1,'parsers.c']]],
   ['parseattrtypeinterval',['parseAttrTypeInterval',['../parsers_8h.html#abc29c7ff0d736d1ab0d9a80b7ff2eab0',1,'parsers.c']]],
   ['parseattrtypetree',['parseAttrTypeTree',['../parsers_8h.html#ab5b90eac71609ac27833b96da19fa57d',1,'parsers.c']]],
-  ['parseattrvalue',['parseAttrValue',['../parsers_8h.html#a137235051082bbc847099b56e0e59273',1,'parsers.c']]],
+  ['parseattrvalue',['parseAttrValue',['../parsers_8h.html#ae2f6a660e8fc746879c84057a2a27e43',1,'parsers.c']]],
   ['parseconfigline',['parseConfigLine',['../parsers_8h.html#a986cbbf2bc6509e1217346c2e1adaad2',1,'parsers.c']]],
   ['parseexample',['parseExample',['../parsers_8h.html#aad08054ce5747601c351fa6965bb42c5',1,'parsers.c']]],
-  ['parseexampleobject',['parseExampleObject',['../parsers_8h.html#afdbbb3b273d6a510aead5c20e0f71753',1,'parsers.c']]],
+  ['parseexampleobject',['parseExampleObject',['../parsers_8h.html#a53ddbc56bfe6f0320f04e39432ccb776',1,'parsers.c']]],
   ['printindent',['printIndent',['../parsers_8h.html#aa13acbba3c52feffbd12fcae8cdf3c9e',1,'parsers.c']]]
 ];
