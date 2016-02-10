1a515bd089100937e94ee643f331e4e70d67bdb6
Gaël Foppolo <me@gaelfoppolo.com>
2016-02-10 19:51:06 +0100

    update generalisation process

--- search/classes_4.js
+++ search/classes_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['object',['Object',['../struct_object.html',1,'']]],
-  ['objectindice',['ObjectIndice',['../struct_object_indice.html',1,'']]],
+  ['objectindex',['ObjectIndex',['../struct_object_index.html',1,'']]],
   ['outattribute',['OutAttribute',['../struct_out_attribute.html',1,'']]],
   ['outenum',['OutEnum',['../struct_out_enum.html',1,'']]],
   ['outobject',['OutObject',['../struct_out_object.html',1,'']]]
